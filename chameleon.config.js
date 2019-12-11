84eae0ee5b774eebcfca05d27757a204dfc9d38d
duanbingyang <duanby0426@163.com>
2019-12-11 11:53:49 +0800

    'apipath'

--- chameleon.config.js
+++ chameleon.config.js
@@ -2,7 +2,7 @@
 // 设置静态资源的线上路径
 const publicPath = 'http://www.hisanlian.com';
 // 设置api请求前缀
-const apiPrefix = 'http://api.hisanlian.com';
+const apiPrefix = 'http://49.234.40.20:3000';
 
 cml.config.merge({
   templateLang: "cml",
